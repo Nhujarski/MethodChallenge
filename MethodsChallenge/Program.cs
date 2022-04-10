d0dc60f851a49ecea580255926546f2eb2934bd9
Nhujarski <52231438+Nhujarski@users.noreply.github.com>
2022-04-10 12:59:52 -0700

    methods challenge for udemy course

---
+++ MethodsChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodsChallenge
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string person1 = "Rob";
+            string person2 = "Mike";
+            string person3 = "Darsh";
+            Greeting(person1);
+            Greeting(person2);
+            Greeting(person3);
+        }
+      
+        public static void Greeting(string name)
+        {
+            Console.WriteLine($" Hey {name}, Whats up My Guy!");
+        }
+    }
+}
